e15308c11439bccdbf82098a478efdf37881b893
Clive <clive.jan@gmail.com>
2020-01-09 12:56:37 -0500

    Add: integer and float

---
+++ data_types/numbers.py
@@ -0,0 +1,24 @@
+# Integer
+print(2 + 3)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+print(3 ** 2)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+# Float
+print(0.1 + 0.1)
+print(2 * 0.1)
+
+# Some float lose precise 
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+# Division alwasy get float result
+print(4 / 2)
+
+# Float appears in expression will get a float result
+print(1 + 2.0)
+print(2 * 3.0)
+print(3.0 ** 2)
