84902bc0bf69d090ad365997a97e3e1341c59a50
Clive <clive.jan@gmail.com>
2020-01-09 14:46:25 -0500

    Update: removing an item from any position in a list and reusing the value

--- data_types/motocycles.py
+++ data_types/motocycles.py
@@ -21,3 +21,7 @@ print(motocycles)
 popped_motocycle = motocycles.pop()
 print(motocycles)
 print(popped_motocycle)
+
+# Removing an item from any position in a list and reusing the value
+first_owned = motocycles.pop(0)
+print(f"The first motocycle I owned was a {first_owned}.")
