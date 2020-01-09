555cb832636534cf2cad483292162479240ddac0
Clive <clive.jan@gmail.com>
2020-01-09 17:03:50 -0500

    Add: [Execcises] 4-1 to 4-2

---
+++ exercises/ex_4_1_pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ['Loaded Baked Potato', 'Macaroni And Cheese', 'Chicken Alfredo']
+
+for pizza in pizzas:
+	print(f"I like {pizza} pizza.")
+print("I really love pizza!")
---
+++ exercises/ex_4_2_animals.py
@@ -0,0 +1,5 @@
+animals = ['tigers', 'lions', 'panthers']
+
+for animal in animals:
+	print(f"A {animal} would make a great pet.")
+print("Any of these animals would make a great pet!")
