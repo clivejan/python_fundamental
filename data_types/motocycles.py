9bd1f17b1777d4f505c2ba48d191313a9eddc334
Clive <clive.jan@gmail.com>
2020-01-09 14:25:59 -0500

    Update: appending elements to the end of list

--- data_types/motocycles.py
+++ data_types/motocycles.py
@@ -4,3 +4,7 @@ print(motocycles)
 # Modifing elements in a list
 motocycles[0] = 'ducati'
 print(motocycles)
+
+# Appending elements to the end of list
+motocycles.append('honda')
+print(motocycles)
