5d45e271aeb9a51fb15f321d49322678fd25e037
Clive <clive.jan@gmail.com>
2020-01-09 12:43:07 -0500

    Add: using quote in string

---
+++ data_types/apostrophe.py
@@ -0,0 +1,7 @@
+# Single quote in between single quotes will cause error
+# message = 'One of Python's strengths is its diverse comminuty.'
+
+# Single quote can be used in between double quotes
+message = "One of Python's strengths is its diverse comminuty."
+
+print(message)
