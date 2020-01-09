a54da88b9bd8836fd23eaea6c07025acf8769af4
Clive <clive.jan@gmail.com>
2020-01-09 13:49:07 -0500

    Update: using individual values from a list

--- data_types/bicycles.py
+++ data_types/bicycles.py
@@ -8,3 +8,7 @@ print(bicycles[0].title())
 print(bicycles[1])
 print(bicycles[3])
 print(bicycles[-1])
+
+# Using individual values from a list
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
