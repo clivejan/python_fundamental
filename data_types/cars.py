e0b3177aba6df39235b31f445a491b43e154ffbe
Clive <clive.jan@gmail.com>
2020-01-09 15:16:21 -0500

    Update: sorting a list in reverse alphabetocal order permanently

--- data_types/cars.py
+++ data_types/cars.py
@@ -3,3 +3,7 @@ cars = ['bmw', 'audi', 'toyota', 'subaru']
 # sorting a list permanently
 cars.sort()
 print(cars)
+
+# sorting a list in reverse alphabetocal order permanently
+cars.sort(reverse=True)
+print(cars)
