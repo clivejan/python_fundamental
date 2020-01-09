7ddf15d7d6f6e87212946e432843d4f597fff9b4
Clive <clive.jan@gmail.com>
2020-01-09 15:22:01 -0500

    Update: sorting a list temporarily

--- data_types/cars.py
+++ data_types/cars.py
@@ -7,3 +7,18 @@ print(cars)
 # sorting a list in reverse alphabetocal order permanently
 cars.sort(reverse=True)
 print(cars)
+
+# sorting a list temporarily
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("\nHere is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the reverse sorted list:")
+print(sorted(cars, reverse=True))
+
+print("\nHere is the original list again:")
+print(cars)
