fa2c333f648a0f5a83f18aca8c650ffc6db52f56
Clive <clive.jan@gmail.com>
2020-01-09 13:45:57 -0500

    Update: accessing individual item

--- data_types/bicycles.py
+++ data_types/bicycles.py
@@ -2,3 +2,9 @@ bicycles = ['trek', 'cannondale', 'redline', 'specoalzed']
 
 # Print the full list structure
 print(bicycles)
+
+# Accessing individual item
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
