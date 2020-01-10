be856f9fd57be8218a980c88fa6eec5a84f230be
Clive <clive.jan@gmail.com>
2020-01-10 08:41:12 -0500

    Add: [Exercises] 4-13

---
+++ exercises/ex_4_13_buffet.py
@@ -0,0 +1,12 @@
+foods = ('Loaded Baked Potato', 'Macaroni And Cheese', 'Chicken Alfredo', 'falafel', 'carrot cake')
+
+for food in foods:
+	print(food)
+
+#foods.append('chicken pie')
+
+foods = ('Loaded Baked Potato', 'Macaroni And Cheese', 'Chicken Alfredo', 'ice cream', 'chicken pie')
+
+print()
+for food in sorted(foods, reverse=True):
+	print(food)
