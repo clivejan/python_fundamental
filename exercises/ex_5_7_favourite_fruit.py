cf0009acccfc2dd8e6e9769d5097df8aca95fdbb
Clive <clive.jan@gmail.com>
2020-01-10 11:16:59 -0500

    Add: [Exercises] 5-3 to 5-7

---
+++ exercises/ex_5_7_favourite_fruit.py
@@ -0,0 +1,16 @@
+favourite_fruit = ['wax apple', 'Guava', 'Tomato']
+
+if 'banana' in favourite_fruit:
+	print("You really like banana!")
+
+if 'wax apple' in favourite_fruit:
+	print("You really like wax apple!")
+
+if 'Guava' in favourite_fruit:
+	print("You really like Guava!")
+
+if 'apple' in favourite_fruit:
+	print("You really like apple!")
+
+if 'Tomato' in favourite_fruit:
+	print("You really like Tomato!")
