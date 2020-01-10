92a009cf515b2512952a82fedbc843090ad47606
Clive <clive.jan@gmail.com>
2020-01-10 12:28:33 -0500

    Update: using multiple lists

--- flow_control/toppings.py
+++ flow_control/toppings.py
@@ -1,10 +1,15 @@
-#requested_toppings = ['mushrooms', 'green peppers', 'extra cheese',]
-requested_toppings = []
+available_toppings = ('mushrooms', 'olives', 'green pepper', 
+	'pepperoni', 'pineapple', 'extra cheese')
+requested_toppings = ['mushrooms', 'green peppers', 
+	'extra cheese', 'job offer']
+#requested_toppings = []
 
 if requested_toppings:
 	for requested_topping in requested_toppings:
 		if requested_topping == 'green peppers':
 			print("Sorry, we are out of greent peppers rigth now.")
+		elif requested_topping not in available_toppings:
+			print(f"Sorry, we don't have {requested_topping}.")
 		else:
 			print(f"Adding {requested_topping}")
 	print("\nFinished making your pizza!")
