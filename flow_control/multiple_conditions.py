b59122bcc734eca2e4db5ab5dd866fcdf0a03c28
Clive <clive.jan@gmail.com>
2020-01-10 09:39:41 -0500

    Add: checking multiple conditions

---
+++ flow_control/multiple_conditions.py
@@ -0,0 +1,16 @@
+# conditional test with and
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+
+# using parentheses to improve readibility
+age_1 = 22
+print((age_0 >= 21) and (age_1 >= 21))
+
+# conditional test with or
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 or age_1 >= 21)
+
+age_0 = 18
+print(age_0 >= 21 or age_1 >= 21)
