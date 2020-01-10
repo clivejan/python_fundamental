eb18bf5e82a7c7ada09662f92c910af9f53542df
Clive <clive.jan@gmail.com>
2020-01-10 12:16:57 -0500

    Update: checking whether a list is empty before looping it

--- flow_control/toppings.py
+++ flow_control/toppings.py
@@ -1,9 +1,13 @@
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese',]
+#requested_toppings = ['mushrooms', 'green peppers', 'extra cheese',]
+requested_toppings = []
 
-for requested_topping in requested_toppings:
-	if requested_topping == 'green peppers':
-		print("Sorry, we are out of greent peppers rigth now.")
-	else:
-		print(f"Adding {requested_topping}")
+if requested_toppings:
+	for requested_topping in requested_toppings:
+		if requested_topping == 'green peppers':
+			print("Sorry, we are out of greent peppers rigth now.")
+		else:
+			print(f"Adding {requested_topping}")
+	print("\nFinished making your pizza!")
+else:
+	print("Are you sure you want a plain pizza?")
 
-print("\nFinished making your pizza!")
