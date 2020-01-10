91f58edb8a56b6dc102ffdf3c4bc72c2ed73385c
Clive <clive.jan@gmail.com>
2020-01-10 08:31:38 -0500

    Update: looping throught all values in a tuple

--- data_types/dimenstions.py
+++ data_types/dimenstions.py
@@ -5,4 +5,8 @@ print(dimensions[0])
 print(dimensions[1])
 
 # try to change the value of tuple will raise an error
-dimensions[0] = 250
+# dimensions[0] = 250
+
+# looping throught all values in a tuple
+for dimension in dimensions:
+	print(dimension)
