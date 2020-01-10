260425cea5488702005d50c677bae0f6361a98a9
Clive <clive.jan@gmail.com>
2020-01-10 10:07:37 -0500

    Add: [Exercises] 5-1 to 5-2

---
+++ exercises/ex_5_1_conditional_tests.py
@@ -0,0 +1,15 @@
+print(f"'audi'.title() == 'Audi' should be True:"
+	f"{'audi'.title() == 'Audi'}")
+
+print(f"'audi'.title() == 'Tesla' should be False:"
+	f"{'audi'.title() == 'Tesla'}")
+
+print(f"'audi'.lower() in ['bmw', 'audi', 'jeep'] should be True: "
+	f"{'audi'.lower() in ['bmw', 'audi', 'jeep']}")
+
+print(f"'Tesla'.lower() in ['bmw', 'audi', 'jeep'] should be False: "
+	f"{'Tesla'.lower() in ['bmw', 'audi', 'jeep']}")
+
+print(f"10 > 9 should be True: {10 > 9}")
+
+print(f"10 < 9 should be False: {10 < 9}")
---
+++ exercises/ex_5_2_more_conditional_tests.py
@@ -0,0 +1,16 @@
+print(f"'Python' == 'Python' should be True: {'Python' == 'Python'}")
+print(f"'Python' != 'Python' should be False: {'Python' != 'Python'}")
+
+print(f"'Python.lower()' == 'Python.lower()' should be True: "
+	f"{'Python.lower()' == 'Python.lower()'}")
+
+print(f"10 >= 9 should be True: {10 >= 9}")
+print(f"10 <= 9 should be False: {10 <= 9}")
+
+print(f"10 >= 9 and 10 <= 9 should be False: {10 >= 9 and 10 <= 9}")
+print(f"10 >= 9 or 10 <= 9 should be True: {10 >= 9 or 10 <= 9}")
+
+print(f"'audi'.lower() in ['bmw', 'audi', 'jeep'] should be True: "
+	f"{'audi'.lower() in ['bmw', 'audi', 'jeep']}")
+print(f"'audi'.lower() not in ['bmw', 'audi', 'jeep'] should be False: "
+	f"{'audi'.lower() not in ['bmw', 'audi', 'jeep']}")
