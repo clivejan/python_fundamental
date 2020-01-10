51873e24e9f3b0c34b98df59b10986d155d53047
Clive <clive.jan@gmail.com>
2020-01-10 13:23:46 -0500

    Update: adding new key-value pairs

--- data_types/alien.py
+++ data_types/alien.py
@@ -5,3 +5,9 @@ print(alien_0['color'])
 
 new_points = alien_0['points']
 print(f"You earned {new_points} points.")
+
+# adding new key-value pairs
+print(f"Original:\t{alien_0}")
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(f"Added:\t\t{alien_0}")
