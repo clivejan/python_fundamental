413e3316b4e819dd2cdc1f51c3bb7a8c4618b5e4
Clive <clive.jan@gmail.com>
2020-01-10 14:20:59 -0500

    Add: usgin get() to set a defaut retuen value

---
+++ data_types/alien_no_points.py
@@ -0,0 +1,7 @@
+alien_0 = {'color': 'green', 'speend': 'slow'}
+
+# access a key that does not exist in a dictionary
+#print(alien_0['points'])
+
+# usgin get() to set a defaut retuen value
+print(alien_0.get('points', 'No point value assigned.'))
