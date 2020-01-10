239f8ca644ba6e68b020aa9e273075989f2770f1
Clive <clive.jan@gmail.com>
2020-01-09 20:25:30 -0500

    Add: slicing a list

---
+++ data_types/players.py
@@ -0,0 +1,13 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print(players[0:3])
+print(players[1:4])
+
+# without a starting index, Python starts at the beginning
+print(players[:4])
+
+# without a ending indexm Python stops at the end of list
+print(players[2:])
+
+# using negative index in slicing
+print(players[-3:])
