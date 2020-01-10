4177644d0819d034d7a2a7d0f2d1bcbd15399e40
Clive <clive.jan@gmail.com>
2020-01-09 21:20:54 -0500

    Add: copying a list by slicing

---
+++ data_types/foods.py
@@ -0,0 +1,11 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+firent_foods = my_foods[:]
+
+my_foods.append('cannoli')
+firent_foods.append('ice cream')
+
+print("My favourite foods are: ")
+print(my_foods)
+
+print("\nMy friend\'s favourite foods are:")
+print(firent_foods)
