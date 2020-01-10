dc27b29892c54185a6ad837158791f4c5b22643e
Clive <clive.jan@gmail.com>
2020-01-10 13:41:33 -0500

    Update: removing key-value pairs

--- data_types/alien.py
+++ data_types/alien.py
@@ -16,3 +16,8 @@ print(f"Added:\t\t{alien_0}")
 print(f"\nThe alien is {alien_0['color']}")
 alien_0['color'] = 'yellow'
 print(f"The alien is now {alien_0['color']}")
+
+# removing key_value pairs
+print(f"\nOriginal:\t{alien_0}")
+del alien_0['points']
+print(f"Deleted:\t{alien_0}")
