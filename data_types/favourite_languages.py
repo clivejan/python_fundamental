00526f54ac256efcaf826be60c665048614fd4d7
Clive <clive.jan@gmail.com>
2020-01-10 14:57:10 -0500

    Update: looping through all the keys in a dictionary

--- data_types/favourite_languages.py
+++ data_types/favourite_languages.py
@@ -11,3 +11,13 @@ favourite_languages = {
 # looping through a dictionary by items()
 for name, language in favourite_languages.items():
 	print(f"{name.title()}'s favourite language is {language}.")
+print()
+
+# looping through all the keys in a dictionary
+friends = ['phli', 'sarah']
+for name in favourite_languages.keys():
+	print(name.title())
+
+	if name in friends:
+		language = favourite_languages[name].title()
+		print(f"\t{name.title()}, I see you love {language}.")
