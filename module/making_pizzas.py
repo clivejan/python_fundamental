1acb9a5ec97317324e2cf3ec7892bdf500e5df53
Clive <clive.jan@gmail.com>
2020-01-11 14:35:55 -0500

    Update: importing specific functions

--- module/making_pizzas.py
+++ module/making_pizzas.py
@@ -1,5 +1,11 @@
 # importing an entire module
-import pizza
+#import pizza
 
-pizza.make_pizza(16, 'sock')
-pizza.make_pizza(12, 'sock', 'booger', 'rock')
+#pizza.make_pizza(16, 'sock')
+#pizza.make_pizza(12, 'sock', 'booger', 'rock')
+
+# importing specific functions
+from pizza import make_pizza
+
+make_pizza(16, 'sock')
+make_pizza(12, 'sock', 'booger', 'rock')
