e9704021206c1aad4cc9476c5a13fb6d1f5d00c6
Clive <clive.jan@gmail.com>
2020-01-11 16:58:35 -0500

    Update: modifying an attribute's value directly

--- classes/car.py
+++ classes/car.py
@@ -21,3 +21,7 @@ class Car:
 my_dream_car = Car('audi', 'q5', 2019)
 print(my_dream_car.get_descriptive_name())
 my_dream_car.read_odometer()
+
+# modifying an attribute's value directly
+my_dream_car.odometer_reading = 23
+my_dream_car.read_odometer()
