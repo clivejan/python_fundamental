f20217b7376022f60824eec23b08011ce72c1be0
Clive <clive.jan@gmail.com>
2020-01-10 20:01:17 -0500

    Add: [Exercises] 7-1 to 7-3

---
+++ exercises/ex_7_2_restaurant_seating.py
@@ -0,0 +1,6 @@
+groups = int(input("How many people are in your dinner group? "))
+
+if groups > 8:
+	print("You'll have to wait for a table.")
+else:
+	print("Your table is ready.")
---
+++ exercises/ex_7_3_multiple_of_ten.py
@@ -0,0 +1,6 @@
+number = int(input("Please enter a number: "))
+
+if number % 10 == 0:
+	print(f"The number {number} is a multiple of ten.")
+else:
+	print(f"The number {number} is a not multiple of ten.")
