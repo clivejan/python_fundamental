4e30b120a57d260396abdfc5675ca25250069f4e
Clive <clive.jan@gmail.com>
2020-01-11 14:40:30 -0500

    Update: using as to give a function as alias

--- module/making_pizzas.py
+++ module/making_pizzas.py
@@ -5,7 +5,13 @@
 #pizza.make_pizza(12, 'sock', 'booger', 'rock')
 
 # importing specific functions
-from pizza import make_pizza
+#from pizza import make_pizza
 
-make_pizza(16, 'sock')
-make_pizza(12, 'sock', 'booger', 'rock')
+#make_pizza(16, 'sock')
+#make_pizza(12, 'sock', 'booger', 'rock')
+
+# using as to give a function an alias
+from pizza import make_pizza as mp
+
+mp(16, 'sock')
+mp(12, 'sock', 'booger', 'rock')
