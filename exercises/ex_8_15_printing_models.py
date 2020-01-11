882db1392471b272fb8ec10bdd7098b82af0f99c
Clive <clive.jan@gmail.com>
2020-01-11 14:58:04 -0500

    Add: [Exercises] 8-15 to 8-16

---
+++ exercises/ex_8_15_printing_models.py
@@ -0,0 +1,8 @@
+import ex_8_15_printing_models_module as ex
+
+# Start with some designs that need to be printed
+unprinted_designs = ['phone case', 'rebot pendnat', 'dodecahedron']
+completed_models = []
+
+ex.print_models(unprinted_designs, completed_models)
+ex.show_completed_models(completed_models)
---
+++ exercises/ex_8_16_imports.py
@@ -0,0 +1,22 @@
+unprinted_designs = ['phone case', 'rebot pendnat', 'dodecahedron']
+completed_models = []
+
+#import ex_8_15_printing_models_module
+#ex_8_15_printing_models_module.print_models(unprinted_designs, completed_models)
+#ex_8_15_printing_models_module.show_completed_models(completed_models)
+
+#from ex_8_15_printing_models_module import print_models, show_completed_models
+#print_models(unprinted_designs, completed_models)
+#show_completed_models(completed_models)
+
+#from ex_8_15_printing_models_module import print_models as pm, show_completed_models as scm
+#pm(unprinted_designs, completed_models)
+#scm(completed_models)
+
+#import ex_8_15_printing_models_module as ex
+#ex.print_models(unprinted_designs, completed_models)
+#ex.show_completed_models(completed_models)
+
+from ex_8_15_printing_models_module import *
+print_models(unprinted_designs, completed_models)
+show_completed_models(completed_models)
