366e81f7bb2bc754382955eec79f8730d249f939
Clive <clive.jan@gmail.com>
2020-01-12 16:56:28 -0500

    Update: using setUp() to prepare attributes for all test cases

--- testinng/test_survey.py
+++ testinng/test_survey.py
@@ -5,24 +5,26 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
 	"""Test for the AnonymousSurvey"""
 
+	def setUp(self):
+		"""
+		Create a survey adn a set of responses for use in all test motheds.
+		"""
+		question = "What language did you first learn to speak? "
+		self.my_sruvey = AnonymousSurvey(question)
+		self.responses = ['Chinese', 'English', 'Python']
+
 	def test_store_single_response(self):
 		"""Test that a single response is stored perperly."""
-		question = "What language did you first learn to speak? "
-		my_sruvey = AnonymousSurvey(question)
-		my_sruvey.store_response('Python')
-		self.assertIn('Python', my_sruvey.response)
+		self.my_sruvey.store_response(self.responses[0])
+		self.assertIn(self.responses[0], self.my_sruvey.response)
 
 	def test_store_three_respinses(self):
 		"""Test that three individual responses are stored perperly."""
-		question = "What language did you first learn to speak? "
-		my_sruvey = AnonymousSurvey(question)
+		for response in self.responses:
+			self.my_sruvey.store_response(response)
 
-		responses = ['Chinese', 'English', 'Python']
-		for response in responses:
-			my_sruvey.store_response(response)
-
-		for response in responses:
-			self.assertIn(response, my_sruvey.response)
+		for response in self.responses:
+			self.assertIn(response, self.my_sruvey.response)
 
 if __name__ == '__main__':
 	unittest.main()
