86a8cc9d4a371e28745d9e43a4e0eaf615af2e5e
Clive <clive.jan@gmail.com>
2020-01-11 19:37:02 -0500

    Update: incrementing an attribute's value through a method

--- classes/car.py
+++ classes/car.py
@@ -28,6 +28,10 @@ class Car:
 		else:
 			print("You cannot roll back an odometer!")
 
+	def increment_odometer(self, miles):
+		"""add the giev amount to the odometer reading"""
+		self.odometer_reading += miles
+
 my_dream_car = Car('audi', 'q5', 2019)
 print(my_dream_car.get_descriptive_name())
 my_dream_car.read_odometer()
@@ -40,3 +44,14 @@ my_dream_car.read_odometer()
 my_dream_car.update_odometer(20)
 my_dream_car.update_odometer(46)
 my_dream_car.read_odometer()
+
+# incrementing an attribute's value through a method
+my_used_car = Car('Hyundai', "Matrix", 2005)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
