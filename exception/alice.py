a3130c1c5010a13961af3428859b1f3bbadecb30
Clive <clive.jan@gmail.com>
2020-01-12 13:02:44 -0500

    Update: analyzing text with try-except-else

--- exception/alice.py
+++ exception/alice.py
@@ -5,3 +5,8 @@ try:
 		content = f.read()
 except FileNotFoundError:
 	print(f"Sorry, the file {filename} does not exist.")
+else:
+	# counnt the approxmate number of words in the file
+	words = content.split()
+	num_words = len(words)
+	print(f"The file {filename} has about {num_words} words.")
