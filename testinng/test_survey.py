8075a11b7ef74406aa418d0e46552477d61ea173
Clive <clive.jan@gmail.com>
2020-01-12 16:49:52 -0500

    Update: adding more tests to test a class

--- testinng/test_survey.py
+++ testinng/test_survey.py
@@ -12,5 +12,17 @@ class TestAnonymousSurvey(unittest.TestCase):
 		my_sruvey.store_response('Python')
 		self.assertIn('Python', my_sruvey.response)
 
+	def test_store_three_respinses(self):
+		"""Test that three individual responses are stored perperly."""
+		question = "What language did you first learn to speak? "
+		my_sruvey = AnonymousSurvey(question)
+
+		responses = ['Chinese', 'English', 'Python']
+		for response in responses:
+			my_sruvey.store_response(response)
+
+		for response in responses:
+			self.assertIn(response, my_sruvey.response)
+
 if __name__ == '__main__':
 	unittest.main()
