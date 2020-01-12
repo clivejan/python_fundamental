4903618ea8f9e1b95a128346f0872b35e645f89d
Clive <clive.jan@gmail.com>
2020-01-11 20:25:55 -0500

    Update: adding new method to attribute instance

--- classes/electric_car.py
+++ classes/electric_car.py
@@ -43,6 +43,13 @@ class Battery:
 		"""print a statement describing the battery size"""
 		print(f"This car has a {self.battery_size}-kWh battery.")
 
+	def get_range(self):
+		"""print a stetement about the range this battery provides"""
+		if self.battery_size == 75:
+			range = 260
+		elif self.battery_size == 100:
+			range = 315
+		print(f"This car can go about {range} miles on a full charge.")
 
 class ElectricCar(Car):
 	"""represent aspects of a car, specific to eletirc vehicles"""
@@ -56,3 +63,4 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', 2009)
 print(my_tesla.get_descriptive_name())
 my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
