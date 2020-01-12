f6db60e000e57a0ff109d06c4fad3a462a6bf762
Clive <clive.jan@gmail.com>
2020-01-12 15:56:25 -0500

    Update: a failing test

--- testinng/test_name_function.py
+++ testinng/test_name_function.py
@@ -8,7 +8,7 @@ class NamesTestCases(unittest.TestCase):
 	def test_first_last_name(self):
 		"""Do names like 'John Doe' work?"""
 		formatted_name = get_formatted_name('john', 'doe')
-		# Verify that a result matches the result you expected to receive.
+		# Verify a result matches the result you expected to receive.
 		self.assertEqual(formatted_name, 'John Doe')
 
 if __name__ == '__main__':
