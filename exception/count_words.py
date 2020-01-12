199f1d55ab80a31404667f595ae8f5abb28b807b
Clive <clive.jan@gmail.com>
2020-01-12 13:11:17 -0500

    Add: working with multiple files

---
+++ exception/count_words.py
@@ -0,0 +1,15 @@
+def count_words(filename):
+	"""counting the approximate number of words in a file"""
+	try:
+		with open(filename, encoding='utf-8') as F:
+			content = F.read()
+	except FileNotFoundError:
+		print(f"Sorry, the file {filename} does not exist.")
+	else:
+		words = content.split()
+		num_words = len(words)
+		print(f"The file {filename} has about {num_words} words.")
+
+filenames = ['alice.txt', 'found_a_job.txt', 'division_calculator.py']
+for filename in filenames:
+	count_words(filename)
