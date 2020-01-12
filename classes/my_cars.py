ccb7b5453606794712ee57f719d52f2c45e38887
Clive <clive.jan@gmail.com>
2020-01-11 21:48:55 -0500

    Add: importing multiple classes from a module

---
+++ classes/my_cars.py
@@ -0,0 +1,7 @@
+from cars import Car, ElectricCar
+
+my_dream_car = Car('Audi', 'Q5', 2019)
+print(my_dream_car.get_descriptive_name())
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
