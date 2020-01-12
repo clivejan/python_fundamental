e1f24fd20a60c27a0067e6a49006eb9308a19c0e
Clive <clive.jan@gmail.com>
2020-01-12 16:06:19 -0500

    Update: adding new tests

--- testinng/test_name_function.py
+++ testinng/test_name_function.py
@@ -11,5 +11,10 @@ class NamesTestCases(unittest.TestCase):
 		# Verify a result matches the result you expected to receive.
 		self.assertEqual(formatted_name, 'John Doe')
 
+	def test_first_middle_last_name(self):
+		"""Do names like 'John Anonymous Doe' work?"""
+		formatted_name = get_formatted_name('john', 'doe', 'anonymous')
+		self.assertEqual(formatted_name, 'John Anonymous Doe')
+
 if __name__ == '__main__':
 	unittest.main()
