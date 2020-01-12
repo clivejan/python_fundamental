3586d4b64600a5166fc4b0807a145f4c93cd71aa
Clive <clive.jan@gmail.com>
2020-01-12 08:03:16 -0500

    Add: importing an entire module

---
+++ classes/my_car_all.py
@@ -0,0 +1,7 @@
+import cars
+
+my_dream_car = cars.Car('Audi', 'Q5', 2019)
+print(my_dream_car.get_descriptive_name())
+
+my_tesla = cars.ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
