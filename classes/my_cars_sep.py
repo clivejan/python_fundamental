35f0d54d24c7e9e92bac8f1e9f3f83639a8fe54f
Clive <clive.jan@gmail.com>
2020-01-12 08:14:05 -0500

    Update: using alias when importing classes

--- classes/my_cars_sep.py
+++ classes/my_cars_sep.py
@@ -1,8 +1,8 @@
 from cars import Car
-from electric_car_sep import ElectricCar
+from electric_car_sep import ElectricCar as EC
 
 my_dream_car = Car('Audi', 'Q5', 2019)
 print(my_dream_car.get_descriptive_name())
 
-my_tesla = ElectricCar('tesla', 'model s', 2019)
+my_tesla = EC('tesla', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
