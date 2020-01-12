d0c1fb266af4deacf9908527a67d72d7df9f948d
Clive <clive.jan@gmail.com>
2020-01-12 10:07:25 -0500

    Update: making a list of lines from a file and prosessing it outside the with block

--- fileio/file_reader.py
+++ fileio/file_reader.py
@@ -9,5 +9,9 @@ with open(filename) as file_object:
 #print(contents.rstrip())
 
 # reading file line by line
-	for line in file_object:
-		print(line.rstrip())
+#	for line in file_object:
+#		print(line.rstrip())
+
+# makeing a list of lines from a file and prosessing it outside the with block
+	lines = file_object.readlines()
+print(lines)
